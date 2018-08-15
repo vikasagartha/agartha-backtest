9623547376c66e96f1c2688886b4ca77a795bfdc
vishaalagartha <vishaalagartha@gmail.com>
2018-08-15 13:01:18 -0700

    Implements sma. Closes #2

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 def destructure(obj, tup):
     return [obj[k] for k in tup]
 
-# TODO
 def sma(window=3):
-    return lambda data:data
+    # split data into window sized chunks
+    stratified_data = [data[i:i+window] for i in range(0, len(data), window)]
+    # compute average of each chunk
+    return lambda data: list(map(lambda sublist: sum(sublist)/len(sublist),
+        stratified_data))
