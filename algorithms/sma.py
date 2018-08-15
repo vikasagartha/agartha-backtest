3609f46cca0a16028791fbd1857da263eb61b3ec
vishaalagartha <vishaalagartha@gmail.com>
2018-08-15 13:12:56 -0700

    sma algorithm index error fix

--- algorithms/sma.py
+++ algorithms/sma.py
@@ -18,4 +18,4 @@ def sma5020(**kwargs):
     sma20 = _sma20(historical_price)
     sma50 = _sma50(historical_price)
 
-    return sma20[index] == sma50[index]
+    return False if (len(sma20)<=index or len(sma50)<=index) else sma20[index] == sma50[index]
