260cd8cf589e89a2d50aca46224954eabff7aac6
vishaalagartha <vishaalagartha@gmail.com>
2018-08-17 11:15:03 -0700

    Adds delta feature to sma algorithm

--- algorithms/sma.py
+++ algorithms/sma.py
@@ -2,6 +2,7 @@ from utils import destructure, sma
 
 _sma20 = sma(20)
 _sma50 = sma(50)
+delta = 10.0
 
 def sma5020(**kwargs):
 
@@ -18,12 +19,13 @@ def sma5020(**kwargs):
     sma20 = _sma20(historical_price)
     sma50 = _sma50(historical_price)
 
-    if len(sma20)>index and len(sma50)>index:
-        # If there is a crossover, we want to buy
-        if sma20[index] == sma50[index]: return n_shares+1
-        # If short sma is greater than long sma, we want to stay
-        elif sma20[index] > sma50[index]: return n_shares
-        # If long sma is greater than short sma, we want to sell
-        elif n_shares>0: return n_shares-1
-
+    try:
+        # check if there is a crossover
+        if abs(sma20[index]-sma50[index]) < delta:
+            if sma20[index+1] > sma50[index+1]:
+                return n_shares+1
+            elif n_shares>0:
+                return n_shares-1
+    except:
+        pass
     return n_shares
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from csv_writer import write_results
 from fetch_data import Data
 
 data = Data('AAPL')
-quote_data = data.get_quote_historicals()
+quote_data = data.get_quote_historicals(interval='day', span='5year', bounds='extended')
 times = list(map(lambda entry: entry['begins_at'], quote_data))
 close_prices = list(map(lambda entry: float(entry['close_price']), quote_data))
-portfolio = backtest(historical_price=close_prices, algo=coinToss)
+portfolio = backtest(historical_price=close_prices, algo=sma5020)
 write_results(portfolio, close_prices, times, 'results')
